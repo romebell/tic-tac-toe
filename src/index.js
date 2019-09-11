57ee230f70765489fe5e2f400d778acc7ea223f9
Rome Bell <romebell@me.com>
2019-09-11 08:03:52 -0700

    Add Game component

--- src/index.js
+++ src/index.js
@@ -36,3 +36,19 @@ class Board extends React.Component {
     ); 
   }
 }
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TO DO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
